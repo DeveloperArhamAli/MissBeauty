be03b6f093ddf63bf0264f9b96135a3eda5fce02
DeveloperArhamAli <muhammadarhamalikhan2009@gmail.com>
2026-06-01 23:33:26 +0500

    add zustand store for user authentication

---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,44 @@
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+
+/**
+ * @typedef {Object} User
+ * @property {string} _id
+ * @property {string} name
+ * @property {string} email
+ */
+
+/**
+ * @typedef {Object} UserStore
+ * @property {User|null} user
+ * @property {boolean} isAuthenticated
+ * @property {function(User):void} login
+ * @property {function():void} logout
+ */
+
+const useAuthStore = create(
+    persist(
+        (set) => ({
+            user: null,
+            isAuthenticated: false,
+
+            login: (userData) => {
+                set(() => ({
+                    userData,
+                    isAuthenticated: true,
+                }))
+            },
+            logout: () => {
+                set(() => ({
+                    user: null,
+                    isAuthenticated: false,
+                }))
+            },
+        })
+    ),
+    {
+        name: "auth-storage",
+    }
+)
+
+export default useAuthStore;
