0b93ab93632531539907cafe9e32c5ae30575ad0
DeveloperArhamAli <muhammadarhamalikhan2009@gmail.com>
2026-06-02 14:12:54 +0500

    refactor: replace redux with zustand for authentication in Login and SignUp components

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,30 +5,25 @@ import { useForm } from "react-hook-form"
 import { useNavigate } from "react-router-dom"
 import { useState } from "react"
 import axios from "axios"
-import { useDispatch } from "react-redux"
-import { login as authLogin } from "../store/authSlice"
+import useAuthStore from "../store/useAuthStore"
 
 function Login() {
+    const { login } = useAuthStore()
+    
     const [error, setError] = useState(null)
     const navigate = useNavigate()
     const { register, handleSubmit } = useForm()
-    const dispatch = useDispatch()
     
     const login = async (data) => {
+        setError(null)
         try {
-            setError(null)
             axios.post('/api/users/login', data)
                 .then((response) => {
                     if (response.data.token) {
                         const userData = response.data.user
-                        dispatch(authLogin(userData))
-                        if (response.data.isAdmin) {
-                            navigate("/admin/dashboard")
-                        } else {
-                            navigate("/")
-                        }
-                    }
-                    if (response.data.errorMessage) {
+                        login(userData)
+                        navigate("/")
+                    } else {
                         setError(response.data.errorMessage)
                     }
                 })
@@ -42,26 +37,26 @@ function Login() {
     return (
         <div className="flex items-center justify-center">
             <div className="w-2/4 min-h-[600px] flex items-center justify-center p-16">
-                    <div className="w-full h-full flex flex-col gap-3 border-2 border-gray-500 rounded-2xl p-10">
-                        <h1 className="text-3xl text-center mt-2">Login to your account.</h1>
-                        <p className="text-center mb-5">Don&apos;t have an account? <Link className="font-bold underline" to="/signup">Sign Up</Link></p>
-                        {error ? <p className="text-red-500 text-center">{error}</p> : null}
-                        <form onSubmit={handleSubmit(login)} className="flex flex-col gap-3">
-                            <Input placeholder="example@123.com" label="Email" type="email" required={true} {...register("email", {
-                                required: true,
-                                validate: {
-                                    matchPattern: (value) =>
-                                        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value)
-                                        ? true
-                                        : "Email address must be a valid address",
-                                }                                
-                            })} />
-                            <Input placeholder="123456789" label="Password" type="password" required={true} {...register("password", {
-                                required: true,
-                            })}/>
-                            <Button label="Login" className="text-center text-lg" type="submit"/>
-                        </form>
-                    </div>
+                <div className="w-full h-full flex flex-col gap-3 border-2 border-gray-500 rounded-2xl p-10">
+                    <h1 className="text-3xl text-center mt-2">Login to your account.</h1>
+                    <p className="text-center mb-5">Don&apos;t have an account? <Link className="font-bold underline" to="/signup">Sign Up</Link></p>
+                    {error ? <p className="text-red-500 text-center">{error}</p> : null}
+                    <form onSubmit={handleSubmit(login)} className="flex flex-col gap-3">
+                        <Input placeholder="example@123.com" label="Email" type="email" required={true} {...register("email", {
+                            required: true,
+                            validate: {
+                                matchPattern: (value) =>
+                                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value)
+                                    ? true
+                                    : "Email address must be a valid address",
+                            }                                
+                        })} />
+                        <Input placeholder="123456789" label="Password" type="password" required={true} {...register("password", {
+                            required: true,
+                        })}/>
+                        <Button label="Login" className="text-center text-lg" type="submit"/>
+                    </form>
+                </div>
             </div>
         </div>
     )
