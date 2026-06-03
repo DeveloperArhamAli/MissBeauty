6e73177946113a4d02d8ff7d131b32464bb559aa
DeveloperArhamAli <muhammadarhamalikhan2009@gmail.com>
2026-06-03 19:04:12 +0500

    update logo font size and change font family to 'sourgumyy'

--- client/src/components/layout/Logo.jsx
+++ client/src/components/layout/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Logo() {
     return (
         <Link to="/" className="absolute left-1/2 -translate-x-1/2">
-            <h1 className="text-3xl font-serif font-bold text-charcoal">
+            <h1 className="text-4xl font-sourgumyy font-bold text-charcoal">
                 SILK<span className="text-gold">HUE</span>
             </h1>
         </Link>
