6e1f73f7b0d429a5bbec7f498431aa32bb488950
DeveloperArhamAli <muhammadarhamalikhan2009@gmail.com>
2026-06-12 11:37:14 +0500

    add eslint config for improved error handling

---
+++ server/eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import importPluginX from 'eslint-plugin-import-x';
+import { defineConfig } from "eslint/config";
+
+export default defineConfig(
+  [
+    {
+      files: ["**/*.{js,mjs,cjs}"], 
+      plugins: {
+        js,
+        "import-x": importPluginX
+      }, 
+      rules: {
+        "import-x/no-unresolved": "error"
+      },
+      extends: ["js/recommended"], 
+      languageOptions: { 
+        globals: globals.browser
+      }
+    },
+  ]
+);
